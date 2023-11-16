908fe2802f71fd7fea74d4fefd45c07c5ef28088
Lukeuke <lukasz.mrowiec10102004@gmail.com>
2023-11-16 23:37:50 +0100

    project and socials section added

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const textContainer = document.getElementById('content');
 const header = document.getElementById('header-path');
-const text = "Welcome to my website!";
+const text = "Hi!";
 
 let index = 0;
 
 function changePath() {
-    header.textContent = `"${window.location.pathname}"`;
+    header.textContent = `"${window.location.pathname.slice(0, window.location.pathname.length)}"`;
 }
 
 function writeText() {
@@ -19,6 +19,19 @@ function writeText() {
         setTimeout(writeText, 100);
     }
 
+}
+
+const openStack = (id) => {
+    let stack = document.getElementById(`project-stack-${id}`)
+
+    if (stack.attributes.getNamedItem('closed').value === "true") {
+        stack.attributes.getNamedItem('closed').value = "false"
+        stack.style.display = 'flex';
+    }
+    else {
+        stack.attributes.getNamedItem('closed').value = "true"
+        stack.style.display = 'none';
+    }
 }
 
 writeText();
